f9606448ec722974a21635d5c31b25d378314b11
Anand Rao <54414858+Anand-Rao745@users.noreply.github.com>
2020-02-22 18:22:39 -0500

    Update SameGameButton.java

--- SameGameButton.java
+++ SameGameButton.java
@@ -8,69 +8,46 @@ import javafx.scene.paint.Color;
  * @author  Anand Rao
  */ 
 public class SameGameButton extends Button{
-  /*Stores the x coordinate of the button*/
+ 
   private int x;
-  /*Stores the y coordinate of the button*/
+  
   private int y;
-  /*Stores true or false if the button has been checked*/
+  
   private boolean hasBeenChecked=false;
   
-  /**
-   * The Constructor
-   * @param y  the y coordinate of the button.
-   * @param x  the x coordinate of the button.
-   * @param hasBeenChecked true or false if the button has been checked.
-   */
+
   public SameGameButton(int y,int x, boolean hasBeenChecked){
     this.y=y;
     this.x=x;
     this.hasBeenChecked=hasBeenChecked;
   }
   
-  /**
-   * Returns the x coordinate of the button.
-   * @return the x coordinate of the button.
-   */
+
   public int getX(){
     return x;
   }
   
-  /**
-   * Changes the x coordinate of the button.
-   * @param x the new x coordinate of the button.
-   */
+
   public void setX(int x){
     this.x=x;
   }
   
-  /**
-   * Returns the y coordinate of the button.
-   * @return the y coordinate of the button.
-   */
+
   public int getY(){
     return y;
   }
   
-  /**
-   * Changes the y coordinate of the button.
-   * @param y the new y coordinate of the button.
-   */ 
+
   public void setY(int y){
     this.y=y;
   }
   
-  /**
-   * Returns the boolean which is true or false.
-   * @return the boolean which is true or false.
-   */
+
   public boolean getHasBeenChecked(){
     return hasBeenChecked;
   }
   
-  /**
-   * Changes the boolean to true or false.
-   * @param hasBeenChecked  the new boolean.
-   */
+
   public void setHasBeenChecked(boolean hasBeenChecked){
     this.hasBeenChecked=hasBeenChecked;
   }
